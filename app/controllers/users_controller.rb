0d71169683f866162191e5de6cb154d084ef52be
koitarou0125-aq <koitaro.takehashi@asia-quest.jp>
2025-05-15 08:34:19 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ def show
   def create
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
