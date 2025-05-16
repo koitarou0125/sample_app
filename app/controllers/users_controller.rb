d69474e7ced800f9003e2f0413bbed3038d8d1d4
koitarou0125-aq <koitaro.takehashi@asia-quest.jp>
2025-05-16 10:50:53 +0000

    0516-1950

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:index, :edit, :update]
+before_action :correct_user,   only: [:edit, :update]
+
+def index
+    @users = User.paginate(page: params[:page])
+  end
 
 def show
     @user = User.find(params[:id])
@@ -20,10 +26,38 @@ def show
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+      # 更新に成功した場合を扱う
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
     def remember(user)
@@ -13,7 +14,10 @@ module SessionsHelper
 
   def current_user
   if (user_id = session[:user_id])
-    @current_user ||= User.find_by(id: user_id)
+    user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
   elsif (user_id = cookies.encrypted[:user_id])
     user = User.find_by(id: user_id)
     if user && user.authenticated?(:remember, cookies[:remember_token])
@@ -22,6 +26,10 @@ module SessionsHelper
     end
   end
 end
+
+def current_user?(user)
+    user && user == current_user
+  end
 
   def logged_in?
     !current_user.nil?
@@ -37,6 +45,10 @@ def log_out
   forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
